6b5ff7bd7ffb90bc58ddefde32af94b735b14b2d
grand3680 <kirill.burtsev07@gmail.com>
2024-05-21 23:14:44 +0300

    graph render update

--- src/components/graph/classes/drawingGraph.ts
+++ src/components/graph/classes/drawingGraph.ts
@@ -14,10 +14,8 @@ export function drawGraph(
 ): void {
   var scale = this.scale;
   // gap to x in function div to scale and next to tuple size [width, heigh]
-  var { x: gapX, y: gapY } = this.size.cdiv(scale).cdiv(this.size);
-
   var showVal = this.size.cdiv(scale);
-
+  var { x: gapX, y: gapY } = this.size.cdiv(scale).cdiv(this.size);
   // this.drawIntersection();
 
   var dX = this.offsetX;
@@ -28,14 +26,29 @@ export function drawGraph(
   var size = this.sizeAxis;
 
   // time val to 1/2
-  const { x: X, y: Y } = showVal;
+  const { x: X, y: Y } = showVal.cdiv(2);
   if (funcG.typeFun === 'x') {
     this.ctx.beginPath();
 
     var first = true;
+    var previousGraphValue = null;
+    const jumpThreshold = Math.abs(-Y-aY)/scale;
+
     for (let y = precision(-Y - aY - size, size); y <= Y - aY; y += gapY) {
-      this.ctx[first ? 'moveTo' : 'lineTo'](funcG.graphFormula(y / 10) * 10, y);
+      let graphValue = funcG.graphFormula(-y / 10) * 10;
+
+      if (!isFinite(graphValue)) {
+        first = true;
+        continue;
+      }
+
+      if (previousGraphValue !== null && (Math.abs(graphValue - previousGraphValue) > jumpThreshold)) {
+        first = true;
+      }
+      this.ctx[first ? 'moveTo' : 'lineTo'](graphValue, y);
       first = false;
+
+      previousGraphValue = graphValue;
     }
 
     this.ctx.strokeStyle = funcG.color;
@@ -47,15 +60,28 @@ export function drawGraph(
     this.ctx.beginPath();
 
     var first = true;
+    var previousGraphValue = null;
+    const jumpThreshold = Math.abs(-X-aX)/scale;
+
     for (let x = precision(-X - aX - size, size); x <= X - aX; x += gapX) {
-      this.ctx[first ? 'moveTo' : 'lineTo'](x, -funcG.graphFormula(x / 10) * 10);
+      let graphValue = -funcG.graphFormula(x / 10) * 10;
+
+      if (!isFinite(graphValue)) {
+        first = true;
+        continue;
+      }
+
+      if (previousGraphValue !== null && (Math.abs(graphValue - previousGraphValue) > jumpThreshold)) {
+        first = true;
+      }
+      this.ctx[first ? 'moveTo' : 'lineTo'](x, graphValue);
       first = false;
+
+      previousGraphValue = graphValue;
     }
 
     this.ctx.strokeStyle = funcG.color;
     this.ctx.stroke();
     this.ctx.closePath();
   }
-
-
 }
